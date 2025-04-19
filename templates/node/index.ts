c5dccb57bb20f58f5fc9249ba29aef79eb789de7
Todor Andonov <todor.andonov@payhawk.com>
2025-04-20 00:38:16 +0300

    chore(formatting): unify biome configs

--- templates/node/index.ts
+++ templates/node/index.ts
@@ -1,41 +1,41 @@
-import { randomInt } from "node:crypto";
-import fs from "node:fs";
-import { parseArgs, styleText } from "node:util";
+import { randomInt } from 'node:crypto';
+import fs from 'node:fs';
+import { parseArgs, styleText } from 'node:util';
 
 interface User {
-	id: string;
-	email: string;
+    id: string;
+    email: string;
 }
 
 const args = parseArgs({
-	options: {
-		url: {
-			type: "string",
-		},
-		dry: {
-			type: "boolean",
-			default: true,
-		},
-	},
+    options: {
+        url: {
+            type: 'string',
+        },
+        dry: {
+            type: 'boolean',
+            default: true,
+        },
+    },
 });
 
 const user = (await fetch(`https://dummyjson.com/users/${randomInt(100)}`).then(
-	(data) => data.json(),
+    (data) => data.json(),
 )) as User;
 
 const data = {
-	user: {
-		id: user.id,
-		email: user.email,
-	},
-	args: { ...args.values },
-	env: { token: process.env.API_TOKEN },
+    user: {
+        id: user.id,
+        email: user.email,
+    },
+    args: { ...args.values },
+    env: { token: process.env.API_TOKEN },
 };
 
-fs.writeFileSync("input.json", JSON.stringify(data, null, 2));
+fs.writeFileSync('input.json', JSON.stringify(data, null, 2));
 
 console.log(
-	styleText("green", "Execution completed."),
-	"\n",
-	JSON.stringify({ data }, null, 2),
+    styleText('green', 'Execution completed.'),
+    '\n',
+    JSON.stringify({ data }, null, 2),
 );
