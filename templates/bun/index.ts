fc7761abc8b16e0e0ded9e36a1c609201d8b16bf
Todor Andonov <todor.andonov@payhawk.com>
2025-04-20 00:36:34 +0300

    feat(bun): add formatting

--- templates/bun/index.ts
+++ templates/bun/index.ts
@@ -1,42 +1,40 @@
-import { randomInt } from "crypto";
-import fs from "fs";
-import { parseArgs, styleText } from "util";
+import { randomInt } from 'node:crypto';
+import { parseArgs, styleText } from 'node:util';
 
 interface User {
-	id: string;
-	email: string;
+    id: string;
+    email: string;
 }
 
 const args = parseArgs({
-	options: {
-		url: {
-			type: "string",
-		},
-		dry: {
-			type: "boolean",
-			default: true,
-		},
-	},
+    options: {
+        url: {
+            type: 'string',
+        },
+        dry: {
+            type: 'boolean',
+            default: true,
+        },
+    },
 });
 
-const user = await fetch(`https://dummyjson.com/users/${randomInt(100)}`).then(data => data.json()) as User;
+const user = (await fetch(`https://dummyjson.com/users/${randomInt(100)}`).then(
+    (data) => data.json(),
+)) as User;
 
 const data = {
-	user: {
-		id: user.id,
-		email: user.email
-	},
-	args: { ...args.values },
-	env: { token: process.env.API_TOKEN },
+    user: {
+        id: user.id,
+        email: user.email,
+    },
+    args: { ...args.values },
+    env: { token: process.env.API_TOKEN },
 };
 
-fs.writeFileSync(
-	"input.json",
-	JSON.stringify(
-		data,
-		null,
-		2,
-	),
-);
+Bun.write('input.json', JSON.stringify(data, null, 2));
 
-console.log(styleText('green', 'Execution completed.'), '\n', JSON.stringify({ data }, null, 2));
+console.log(
+    styleText('green', 'Execution completed.'),
+    '\n',
+    JSON.stringify({ data }, null, 2),
+);
