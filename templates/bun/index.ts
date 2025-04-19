3a35997da3641032d8b82d596e55350a451c8299
Todor Andonov <todor.andonov@payhawk.com>
2025-04-19 23:54:21 +0300

    feat(bun): add bun

---
+++ templates/bun/index.ts
@@ -0,0 +1,42 @@
+import { randomInt } from "crypto";
+import fs from "fs";
+import { parseArgs, styleText } from "util";
+
+interface User {
+	id: string;
+	email: string;
+}
+
+const args = parseArgs({
+	options: {
+		url: {
+			type: "string",
+		},
+		dry: {
+			type: "boolean",
+			default: true,
+		},
+	},
+});
+
+const user = await fetch(`https://dummyjson.com/users/${randomInt(100)}`).then(data => data.json()) as User;
+
+const data = {
+	user: {
+		id: user.id,
+		email: user.email
+	},
+	args: { ...args.values },
+	env: { token: process.env.API_TOKEN },
+};
+
+fs.writeFileSync(
+	"input.json",
+	JSON.stringify(
+		data,
+		null,
+		2,
+	),
+);
+
+console.log(styleText('green', 'Execution completed.'), '\n', JSON.stringify({ data }, null, 2));
